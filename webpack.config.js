ac060a551abdee5adac2cf9ca516030d78eccebd
Lucas Comamala <lucascomamala@gmail.com>
2022-08-11 14:53:00 -0600

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
@@ -15,6 +19,9 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   plugins: [
     new HtmlWebpackPlugin({
