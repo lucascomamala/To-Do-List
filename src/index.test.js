b1534a59319a40412c2f52d5ffb999fde086c1a7
Lucas Comamala <lucascomamala@gmail.com>
2022-08-18 14:03:53 -0600

    Add addItem DOM test
    - Added babel config file
    - installed jsdom test environment
    - Removed file that was leftover

--- src/index.test.js
+++ src/index.test.js
@@ -3,6 +3,8 @@
 import Item from "./modules/listItem.js";
 import List from "./modules/list.js";
 
+
+
 describe('Add and remove', () => {
   const list = new List(Item, 'testing list', 'test list sto');
   test('Expect addItem to increase list length', () => {
@@ -14,5 +16,17 @@ describe('Add and remove', () => {
     const listLength = list.list.length;
     list.removeItemByIndex(0);
     expect(list.list.length).toBe(listLength - 1);
+  });
+});
+
+describe("DOM Tests", () => {
+  jest.mock("./index");
+  const list1 = new List(Item, "testing list", "test list sto");
+  test("Expect addItem to add to DOM", () => {
+    document.body.innerHTML = "<div>" + '<ul id="list"></ul>' + "</div>";
+    const item = (list1.addItem("Task 1")).template();
+    document.querySelector("#list").appendChild(item);
+    const check = document.querySelectorAll("#list li");
+    expect(check).toHaveLength(1);
   });
 });
